598f646d0fbe08a9a79554506b0fc19c631c7f73
berkawcollin0-del <berkawcollin0@gmail.com>
2026-05-31 09:12:06 -0500

    Create tickers.py

---
+++ tickers.py
@@ -0,0 +1,22 @@
+# tickers.py
+WATCHLIST = [
+    'AAPL', 'MSFT', 'NVDA', 'AMZN', 'GOOGL', 'META', 'BRK-B', 'TSLA', 'AVGO', 'JPM',
+    'V', 'UNH', 'JNJ', 'PG', 'MA', 'HD', 'CVX', 'MRK', 'ABBV', 'KO', 'PEP', 'XOM',
+    'ADBE', 'COST', 'WMT', 'BAC', 'CRM', 'AMD', 'NFLX', 'CSCO', 'TMO', 'DHR', 'INTC',
+    'CMCSA', 'DIS', 'ABT', 'PFE', 'VZ', 'WFC', 'ORCL', 'LIN', 'UPS', 'INTU', 'MCD',
+    'QCOM', 'CAT', 'GE', 'BA', 'RTX', 'TXN', 'AMGN', 'HON', 'GS', 'IBM', 'PLD',
+    'AMAT', 'UNP', 'LOW', 'DE', 'NEE', 'AXP', 'ISRG', 'BKNG', 'SPGI', 'BLK', 'MDLZ',
+    'SYK', 'GILD', 'ADP', 'CB', 'MMC', 'LRCX', 'ADI', 'CI', 'BDX', 'REGN', 'C', 'EL',
+    'MU', 'CSX', 'FISV', 'SNPS', 'KLAC', 'PNC', 'ZTS', 'ITW', 'EOG', 'MCK', 'AON',
+    'SHW', 'PH', 'CNC', 'WM', 'MS', 'USB', 'ETN', 'FDX', 'APD', 'NSC', 'CDNS', 'MMM',
+    'CL', 'GD', 'SNY', 'TGT', 'SO', 'BSX', 'PGR', 'CME', 'DUK', 'CBRE', 'PYPL', 'ADSK',
+    'MDT', 'MRVL', 'TEAM', 'KHC', 'DXCM', 'BIIB', 'CTAS', 'IT', 'HCA', 'HUM', 'IDXX',
+    'ECL', 'FTNT', 'ROK', 'AFL', 'WEC', 'FCX', 'O', 'PAYX', 'D', 'NXPI', 'CPT', 'VRSK',
+    'AJG', 'CTSH', 'MNST', 'EXC', 'YUM', 'MTD', 'CARR', 'PPL', 'PHM', 'AMP', 'FAST',
+    'TEL', 'ANSS', 'MSI', 'PPG', 'KMB', 'GLW', 'WBA', 'GWW', 'SWK', 'WST', 'DHI',
+    'JCI', 'BKR', 'LEN', 'HLT', 'CMI', 'ZBH', 'NVR', 'TDG', 'ED', 'PCAR', 'TSN',
+    'STZ', 'IFF', 'MCHP', 'PSX', 'ROST', 'FITB', 'AEE', 'RMD', 'VMC', 'XYL', 'HPQ',
+    'SRE', 'FE', 'AEP', 'CINF', 'NTRS', 'EXPD', 'KEYS', 'MTB', 'DFS', 'HIG', 'HBAN',
+    'KEY', 'CF', 'WRB', 'DOV', 'CHD', 'SJM', 'TT', 'SYY', 'QRVO', 'KMI', 'DAL',
+    'LUV', 'UAL', 'KSU', 'WDC', 'STT', 'BBY', 'JBHT', 'LEG', 'POOL', 'TER', 'DXC'
+]
